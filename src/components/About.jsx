b92dd394cb1682b1e7e3846aa12fae04ce1e17d6
Redo76 <redouane.hamadache1@gmail.com>
2022-06-17 12:24:53 +0200

    Création du components About

---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const About = () => {
+  return (
+    <>
+        <Container style={{"margin-top": "50px"}}>
+            <h1>Qui sommes-nous ?</h1>
+            <p>Lorem200</p>
+            <h1>Notre spécialité</h1>
+            <Row>
+                <Col md={6}>
+                    <p>Lorem200</p>
+                </Col>
+                <Col md={6}>
+                    <p>200</p>
+                </Col>
+            </Row>
+            <Row>
+                <h1>Le Chef</h1>
+                <Col md={3}>
+                    <p>Lorem100</p>
+                </Col>
+                <Col md={3}>
+                    <p>Lorem100</p>
+                </Col>
+                <Col md={3}>
+                    <p>Lorem100</p>
+                </Col>
+                <Col md={3}>
+                    <p>Lorem100</p>
+                </Col>
+            </Row>
+        </Container>
+    </>
+  )
+}
+
+export default About
